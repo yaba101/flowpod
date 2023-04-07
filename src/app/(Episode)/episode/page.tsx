d62efc1847aedf1aab0589cd77c46a7d7d7c648e
Yeabsira Mekuria <Yabumek46@gmail.com>
2023-04-08 00:11:19 +0300

    update episode list page UI

--- src/app/(Episode)/episode/page.tsx
+++ src/app/(Episode)/episode/page.tsx
@@ -23,16 +23,27 @@ async function getEpisodes() {
 const page = async () => {
 	const episodes = await getEpisodes()
 	return (
-		<div className='pt-16 pb-12 sm:pb-4 lg:pt-12 bg-slate-950 '>
-			<Container>
-				<h1 className='text-2xl font-bold leading-7 text-gray-200'>Episodes</h1>
-			</Container>
-			<div className='divide-y divide divide-slate-600 sm:mt-4 lg:mt-8 lg:border-t lg:border-slate-600'>
-				{episodes.map((episode) => (
-					<EpisodeItem key={episode.id} episode={episode} />
-				))}
-			</div>
-		</div>
+		<>
+			<section className='dark:bg-gray-800 dark:text-gray-100'>
+				<div className='container max-w-5xl px-4 py-12 mx-auto'>
+					<div className='grid gap-4 mx-4 sm:grid-cols-12'>
+						<div className='col-span-12 sm:col-span-3'>
+							<div className='text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-violet-400'>
+								<h3 className='text-3xl font-semibold'>Episodes</h3>
+								{/* <span className='text-sm font-bold tracking-wider uppercase dark:text-gray-400'>
+									Vestibulum diam nunc
+								</span> */}
+							</div>
+						</div>
+						<div className='relative col-span-12 px-4 space-y-6 sm:col-span-9'>
+							{episodes.map((episode) => (
+								<EpisodeItem key={episode.id} episode={episode} />
+							))}
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
 	)
 }
 
