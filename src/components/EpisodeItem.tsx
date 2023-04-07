7550840adbbc3b07d6246f8c4f1aac1cc2e9a3bf
Yeabsira Mekuria <Yabumek46@gmail.com>
2023-04-07 17:48:17 +0300

    add play and pause button to episode item

--- src/components/EpisodeItem.tsx
+++ src/components/EpisodeItem.tsx
@@ -57,6 +57,10 @@ export default function EpisodeItem(episode: EpisodeProps) {
 							aria-label={`${player.playing ? 'Pause' : 'Play'} episode ${
 								episode.title
 							}`}>
+							<PlayPauseIcon
+								playing={player.playing}
+								className='h-2.5 w-2.5 fill-current'
+							/>
 							<span className='ml-3' aria-hidden='true'>
 								Listen
 							</span>
@@ -76,5 +80,21 @@ export default function EpisodeItem(episode: EpisodeProps) {
 				</div>
 			</Container>
 		</article>
+	)
+}
+
+function PlayPauseIcon({ playing, ...props }: any) {
+	return (
+		<svg aria-hidden='true' viewBox='0 0 10 10' fill='none' {...props}>
+			{playing ? (
+				<path
+					fillRule='evenodd'
+					clipRule='evenodd'
+					d='M1.496 0a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5H2.68a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5H1.496Zm5.82 0a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5H8.5a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5H7.316Z'
+				/>
+			) : (
+				<path d='M8.25 4.567a.5.5 0 0 1 0 .866l-7.5 4.33A.5.5 0 0 1 0 9.33V.67A.5.5 0 0 1 .75.237l7.5 4.33Z' />
+			)}
+		</svg>
 	)
 }
