86c81cab812fdc2bbcb9241b03f9a8afe7c466c4
Yeabsira Mekuria <Yabumek46@gmail.com>
2023-04-09 13:32:14 +0300

    add livestream page with coming soon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body>
+			<body className='h-screen'>
 				<AudioProvider>{children}</AudioProvider>
 			</body>
 		</html>
