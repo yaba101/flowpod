27eb6a1502bd415e23360aad25757ffa52cccc44
Yeabsira Mekuria <Yabumek46@gmail.com>
2023-04-09 13:37:52 +0300

    fetch episode/podcast detail page from contentful cms

--- src/app/(Episode)/episodes/[episode]/page.tsx
+++ src/app/(Episode)/episodes/[episode]/page.tsx
@@ -1,11 +1,12 @@
 import EpisodePage from '@/components/EpisodePage'
-import React from 'react'
-import { parse } from 'rss-to-json'
 import { notFound } from 'next/navigation'
+import { client } from '@/lib/contentful/client'
+import { EpisodeTypeProps } from '../page'
 
 type EpisodeProps = {
 	id: string
 	title: string
+	slug: string
 	audio: {
 		src: string
 		type: string
@@ -15,22 +16,26 @@ type EpisodeProps = {
 	content: string
 }
 
-async function getEpisode(params: string) {
-	let feed = await parse('https://their-side-feed.vercel.app/api/feed')
-	let episode = feed.items
-		.map(({ id, title, description, content, enclosures, published }) => ({
-			id: id.toString(),
-			title: `${id}: ${title}`,
-			description,
-			content,
-			published,
-			audio: enclosures.map((enclosure: { url: string; type: string }) => ({
-				src: enclosure.url,
-				type: enclosure.type,
-			}))[0],
-		}))
-		.find(({ id }) => id === params)
-	return episode as EpisodeProps
+async function getEpisode(params: string): Promise<EpisodeProps> {
+	const response = await client.getEntries({ content_type: 'episodes' })
+	const episode: EpisodeProps = await response.items
+		.map((item: EpisodeTypeProps) => {
+			return {
+				id: item.fields.podcastAudio.sys.id,
+				title: item.fields.title,
+				slug: item.fields.slug,
+				published: item.fields.podcastAudio.sys.createdAt,
+				description: item.fields.description,
+				audio: {
+					src: item.fields.podcastAudio.fields.file.url,
+					type: item.fields.podcastAudio.fields.file.contentType,
+				},
+				content: item.fields.content,
+			}
+		})
+		.find(({ slug }: { slug: string }) => slug === params)
+
+	return episode
 }
 
 const page = async ({ params }: { params: { episode: string } }) => {
--- src/components/EpisodePage.tsx
+++ src/components/EpisodePage.tsx
@@ -2,11 +2,15 @@
 import { useMemo } from 'react'
 import { useAudioPlayer } from '@/components/AudioProvider'
 import { Container } from '@/components/Container'
-import { FormattedDate } from '@/components/FormattedDate'
+
 import { PlayButton } from '@/components/player/PlayButton'
+import Markdoc from '@markdoc/markdoc'
+import { format } from 'date-fns'
+
 type EpisodeProps = {
 	id: string
 	title: string
+	slug: string
 	audio: {
 		src: string
 		type: string
@@ -18,6 +22,11 @@ type EpisodeProps = {
 
 export default function EpisodePage({ episode }: { episode: EpisodeProps }) {
 	let date = new Date(episode.published)
+	const ast = Markdoc.parse(episode.content)
+
+	const content = Markdoc.transform(ast)
+
+	const html = Markdoc.renderers.html(content)
 
 	let audioPlayerData = useMemo(
 		() => ({
@@ -26,7 +35,7 @@ export default function EpisodePage({ episode }: { episode: EpisodeProps }) {
 				src: episode.audio.src,
 				type: episode.audio.type,
 			},
-			link: `/episodes/${episode.id}`,
+			link: `/episodes/${episode.slug}`,
 		}),
 		[episode]
 	)
@@ -34,7 +43,7 @@ export default function EpisodePage({ episode }: { episode: EpisodeProps }) {
 
 	return (
 		<>
-			<article className='py-16 bg-gray-800 lg:py-36'>
+			<article className='py-5 bg-gray-800 lg:py-20 overflow-hidden'>
 				<Container>
 					<header className='flex flex-col'>
 						<div className='flex items-center gap-6'>
@@ -43,10 +52,9 @@ export default function EpisodePage({ episode }: { episode: EpisodeProps }) {
 								<h1 className='mt-2 text-4xl font-bold text-slate-200'>
 									{episode.title}
 								</h1>
-								<FormattedDate
-									date={date}
-									className='order-first font-mono text-sm leading-7 text-slate-300'
-								/>
+								<time className='order-first font-mono text-sm leading-7 text-slate-500'>
+									{format(date, 'h:mm a')}
+								</time>
 							</div>
 						</div>
 						<p className='mt-3 ml-24 text-lg font-medium leading-8 text-slate-300'>
@@ -55,8 +63,8 @@ export default function EpisodePage({ episode }: { episode: EpisodeProps }) {
 					</header>
 					<hr className='my-12 border-gray-200' />
 					<div
-						className=" text-gray-200 prose prose-slate mt-14 [&>h2]:mt-12 [&>h2]:flex [&>h2]:items-center [&>h2]:font-mono [&>h2]:text-sm [&>h2]:font-medium [&>h2]:leading-7 [&>h2]:text-slate-300 [&>h2]:before:mr-3 [&>h2]:before:h-3 [&>h2]:before:w-1.5 [&>h2]:before:rounded-r-full [&>h2]:before:bg-cyan-200 [&>ul]:mt-6 [&>ul]:list-['\2013\20'] [&>ul]:pl-5 [&>h2:nth-of-type(3n+2)]:before:bg-indigo-200 [&>h2:nth-of-type(3n)]:before:bg-violet-200"
-						dangerouslySetInnerHTML={{ __html: episode.content }}
+						className=' text-gray-300 prose prose-slate h-screen '
+						dangerouslySetInnerHTML={{ __html: html }}
 					/>
 				</Container>
 			</article>
